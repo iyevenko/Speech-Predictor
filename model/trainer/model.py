c3a9f0a49ce9251a70049a04cb92bfc9b1955c70
abatek <44042291+abatek@users.noreply.github.com>
2020-12-22 13:44:56 -0500

    updated model

--- model/trainer/model.py
+++ model/trainer/model.py
@@ -1,39 +1,45 @@
 import numpy as np
 
-import tensorflow_datasets as tfds
 import tensorflow as tf
-
-tfds.disable_progress_bar()
-
-def input_fn(batch_size):
-    dataset, info = tfds.load('imdb_reviews', with_info=True,
-                              as_supervised=True)
-    train_dataset, test_dataset = dataset['train'], dataset['test']
-
-    BUFFER_SIZE = 10000
-
-    train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
-    test_dataset = test_dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
-
-    return train_dataset
+import os
 
 
-def create_keras_model(encoder):
-    model = tf.keras.Sequential([
-        encoder,
-        tf.keras.layers.Embedding(
-            input_dim=len(encoder.get_vocabulary()),
-            output_dim=64,
-            # Use masking to handle the variable sequence lengths
-            mask_zero=True),
-        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(1)
-    ])
+def input_fn(buffer_size, data_path, min_sentence_length):
+    """
 
-    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  optimizer=tf.keras.optimizers.Adam(1e-4),
-                  metrics=['accuracy'])
+    :return: tuple of n previous words, and next word
+    """
 
-    return model
+    # create generator for tensorflow dataset
+    def generator():
+        from collections import deque
 
+        for text_file in os.listdir(data_path):
+            word_buffer = deque(maxlen=buffer_size)
+            with open(os.path.join(data_path, text_file), encoding='utf-8') as text:
+                lines = text.readlines()
+                for line_str in lines:
+                    line = line_str.split()
+                    if len(line) > min_sentence_length:
+                        word_buffer.append(line[0])
+                        for i in range(1, len(line)):
+                            yield tf.stack(word_buffer), line[i]
+                            word_buffer.append(line[i])
+
+    dataset = tf.data.Dataset.from_generator(generator,
+                                             output_types=(tf.string, tf.string),
+                                             # output_shapes=([None], 1)
+                                             )
+    return dataset
+
+
+if __name__ == '__main__':
+    dataset = input_fn(3, os.path.join('..', '..', 'ANC_training_data'), 10)
+    import time
+    start_time = time.time()
+    print(len(list(dataset.as_numpy_iterator())))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+def create_keras_model():
+    pass
