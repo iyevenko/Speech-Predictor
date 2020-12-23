b24ebc782d2f741f920615047a6e209fadea81e1
Ivan Yevenko <iyevenko@gmail.com>
2020-12-22 19:36:50 -0500

    Updated generator dataset and added TextVectorization

--- model/trainer/model.py
+++ model/trainer/model.py
@@ -1,44 +1,57 @@
-import numpy as np
+import os
+import random
+from collections import deque
 
 import tensorflow as tf
-import os
 
 
-def input_fn(buffer_size, data_path, min_sentence_length):
+def input_fn(buffer_size, data_path, min_sentence_length, dataset_fraction):
     """
 
     :return: tuple of n previous words, and next word
     """
 
     # create generator for tensorflow dataset
-    def generator():
-        from collections import deque
+    text_files = os.listdir(data_path)
+    text_files = [os.path.join(data_path, f) for f in text_files]
+    num_files = len(text_files)
+    random.shuffle(text_files)
+    text_files = text_files[:int(num_files * dataset_fraction)]
 
-        for text_file in os.listdir(data_path):
-            word_buffer = deque(maxlen=buffer_size)
-            with open(os.path.join(data_path, text_file), encoding='utf-8') as text:
+    texts = tf.data.TextLineDataset(text_files)
+    encoder = tf.keras.layers.experimental.preprocessing.TextVectorization(max_tokens=10000)
+    encoder.adapt(texts)
+
+    def generator():
+        for text_file in text_files:
+            with open(text_file, encoding='utf-8') as text:
                 lines = text.readlines()
                 for line_str in lines:
+                    word_buffer = deque(maxlen=buffer_size)
                     line = line_str.split()
                     if len(line) > min_sentence_length:
                         word_buffer.append(line[0])
                         for i in range(1, len(line)):
-                            yield tf.stack(word_buffer), line[i]
+                            yield tf.convert_to_tensor([" ".join(word_buffer)]), tf.convert_to_tensor([line[i]])
                             word_buffer.append(line[i])
 
-    dataset = tf.data.Dataset.from_generator(generator,
-                                             output_types=(tf.string, tf.string),
-                                             # output_shapes=([None], 1)
-                                             )
-    return dataset
+    dataset = tf.data.Dataset.from_generator(generator, output_types=(tf.string, tf.string))
+
+    return dataset, encoder
 
 
 if __name__ == '__main__':
-    dataset = input_fn(3, os.path.join('..', '..', 'ANC_training_data'), 10)
-    import time
-    start_time = time.time()
-    print(len(list(dataset.as_numpy_iterator())))
-    print("--- %s seconds ---" % (time.time() - start_time))
+    dataset, encoder = input_fn(3, os.path.join('..', '..', 'ANC_training_data'), 10)
+    # dataset = dataset.shuffle(buffer_size=10000)
+    # start_time = time.time()
+    dataset_numpy = dataset.as_numpy_iterator()
+    # print(len(list(dataset_numpy)))
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    vocab_iter = iter(encoder.get_vocabulary())
+    while not input() == 'q':
+        # next_example = next(dataset_numpy)
+        # print(next_example)
+        print(next(vocab_iter))
 
 
 def create_keras_model():
