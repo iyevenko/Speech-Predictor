60cab84392a08b9eb73bd86f1404f5499b8aeb9e
Ivan Yevenko <iyevenko@gmail.com>
2020-12-22 22:53:41 -0500

    Unfinished BERT implementation - doesn't work

--- model/trainer/model.py
+++ model/trainer/model.py
@@ -3,6 +3,7 @@ import random
 from collections import deque
 
 import tensorflow as tf
+import tensorflow_text
 import tensorflow_hub as hub
 
 def input_fn(buffer_size, data_path, min_sentence_length, dataset_fraction, vocabulary_size):
@@ -46,7 +47,7 @@ if __name__ == '__main__':
 
     # hub.load(os.path.abspath('preprocess'))
     # print(os.path.abspath('preprocess'))
-    bert_preprocess_model = tf.keras.models.load_model(os.path.join('..' ,'preprocess'))
+    # bert_preprocess_model = tf.keras.models.load_model(os.path.join('..' ,'preprocess'))
     # text_test = ['this is such an amazing movie!']
     # text_preprocessed = bert_preprocess_model(text_test)
 
@@ -69,6 +70,33 @@ if __name__ == '__main__':
     #     print(encoder(next_example[0]))
     #     # print(next(vocab_iter))
 
+    preprocess = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2', trainable=True)
+    bert = hub.KerasLayer('https://tfhub.dev/google/experts/bert/wiki_books/mnli/2')
+
+    # text_test = ["MY NAME IS FLYNN. Don't call me Walt Jr!"]
+    # text_preprocessed = preprocess(text_test)
+    #
+    # print(f'Keys       : {list(text_preprocessed.keys())}')
+    # print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
+    # print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+    # print(f'Input Mask : {text_preprocessed["input_mask"][:]}')
+    # print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
+    #
+    # bert_results = bert(text_preprocessed)
+    #
+    # print(f'Pooled Outputs Shape:{bert_results["pooled_output"].shape}')
+    # print(f'Pooled Outputs Values:{bert_results["pooled_output"][0, :12]}')
+    # print(f'Sequence Outputs Shape:{bert_results["sequence_output"].shape}')
+    # print(f'Sequence Outputs Values:{bert_results["sequence_output"][0, :12]}')
+    print("Finished loading weights")
+    encoding1 = bert(preprocess(['master']))['pooled_output']
+    encoding2 = bert(preprocess(['slave']))['pooled_output']
+    encoding3 = bert(preprocess(['toys']))['pooled_output']
+    print(tf.keras.losses.MSE(encoding1, encoding2))
+    print(tf.keras.losses.MSE(encoding1, encoding3))
+    print(tf.keras.losses.MSE(encoding2, encoding3))
+
+
 
 class BERTModel(tf.keras.Model):
     def __init__(self, text_encoder, buffer_size, *args, **kwargs):
@@ -77,7 +105,27 @@ class BERTModel(tf.keras.Model):
         self.embedding = tf.keras.layers.Embedding(input_dim=buffer_size, output_dim=128)
         self.preprocess = hub.load('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2')
         self.bert = hub.load('https://tfhub.dev/google/experts/bert/wiki_books/mnli/2')
+        self.lstm = tf.keras.layers.LSTM(128, return_sequences=False)
+        self.dense = tf.keras.layers.Dense(768, activation='relu')
+        self.softmax = tf.keras.layers.Dense( activation='softmax')
 
 
     def call(self, inputs, training=None, mask=None):
-        pass
+        text_preprocessed = self.preprocess(inputs)
+        bert_results = self.bert(text_preprocessed)
+        pooled_output = bert_results["pooled_output"]
+        sequence_output = bert_results["sequence_output"]
+
+        lstm_output = self.lstm(sequence_output)
+        lstm_output = self.dense(lstm_output)
+        encoded_prediction = tf.keras.layers.Concatenate([lstm_output, pooled_output])
+        encoded_prediction = self.dense(encoded_prediction)
+
+        word_probs = self.softmax(encoded_prediction)
+
+
+        # [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+        # [x, x, x, x, x, x, x, 0, x, x, x, x ,x, x]
+
+
+
